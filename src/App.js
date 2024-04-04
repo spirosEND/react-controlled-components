a1ef4f17c9616e25ba5e0b7d58b0035d6a3c121f
Spiros Vatikiots <spiros.vatikiotis@cyberstream.gr>
2024-04-04 16:47:01 +0300

    fix spaces

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ function App() {
   return (
     <div className="App">
       <ControlledComponent />
+      <br />
       <DropDown />
+      <br />
       <CheckBox />
+      <br />
       <Multiple />
+      <br />
       <MyForm />
     </div>
   );
@@ -98,13 +102,19 @@ function Multiple() {
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="name">Name:</label>
+      <br />
       <input type="text" id="name" name="name" value={formData.name} onChange={handleChange}/>
+      <br />
 
       <label htmlFor="email">Email:</label>
+      <br />
       <input type="email" id="email" name="email" value={formData.email} onChange={handleChange}/>
+      <br />
 
       <label htmlFor="message">Message:</label>
+      <br />
       <textarea id="message" name="message" value={formData.message} onChange={handleChange}/>
+      <br />
 
       <button type="submit">Submit</button>
     </form>
